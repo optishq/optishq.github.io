2ffccea0f92a3d5e7f7707f8ae4b7493a82a06b4
optishq <optishq@gmail.com>
2026-03-18 22:21:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
+function toggleMenu() {
+  const menu = document.getElementById("mobileMenu");
+  menu.classList.toggle("show");
+}
+
 function showSection(sectionId) {
   const sections = document.querySelectorAll("section");
   sections.forEach(sec => sec.classList.remove("active"));
 
   document.getElementById(sectionId).classList.add("active");
 
-  // Close menu on mobile after click
-  document.getElementById("nav-links").classList.remove("show");
-}
-
-function toggleMenu() {
-  document.getElementById("nav-links").classList.toggle("show");
+  // Close menu after click (mobile)
+  document.getElementById("mobileMenu").classList.remove("show");
 }
